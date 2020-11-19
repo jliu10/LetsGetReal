566abdb4381c091c2a926559e57c8d290a501ee3
jliu10 <jliu10@stuy.edu>
2020-11-19 14:35:09 -0500

    tested method equals

---
+++ Tester.java
@@ -0,0 +1,15 @@
+public class Tester{
+  public static void equalsTester(RealNumber x, RealNumber y){
+    System.out.println(x.equals(y));
+  }
+
+  public static void main(String[] args){
+    RealNumber zero=new RealNumber(0);
+    RealNumber one=new RealNumber(1);
+    RealNumber two=new RealNumber(2);
+
+    equalsTester(zero,zero);
+    equalsTester(zero,one);
+    equalsTester(two,zero);
+  }
+}
